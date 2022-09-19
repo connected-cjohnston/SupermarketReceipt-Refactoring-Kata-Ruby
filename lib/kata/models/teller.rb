2704b00c34d0d7cdd340479b171da738ae0b46c4
Chris Johnston <cjohnston@connected.io>
2022-09-19 10:34:18 -0400

    Small refactor to more idiomatic ruby

--- lib/kata/models/teller.rb
+++ lib/kata/models/teller.rb
@@ -11,14 +11,16 @@ class Kata::Teller
 
   def checks_out_articles_from(the_cart)
     receipt = Kata::Receipt.new
-    product_quantities = the_cart.items
-    for pq in product_quantities do
+
+    the_cart.items.each do |pq|
       p = pq.product
       quantity = pq.quantity
       unit_price = @catalog.unit_price(p)
       price = quantity * unit_price
+
       receipt.add_product(p, quantity, unit_price, price)
     end
+
     the_cart.handle_offers(receipt, @offers, @catalog)
 
     receipt
